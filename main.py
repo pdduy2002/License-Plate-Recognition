8312186814db383ed55dc39731a222842acc7a44
khanhlq <lqkhanh195@gmail.com>
2024-12-17 17:28:47 +0700

    Done

--- main.py
+++ main.py
@@ -2,37 +2,74 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
+from ultralytics import YOLO
+from utils import *
+from paddleocr import PaddleOCR, draw_ocr
+import time
 
 def main():
+    if "det_model" not in st.session_state:
+        print("=========Initializing yolo========")
+        st.session_state.det_model = YOLO("model/yolov8_weight.pt")
+    
+    if "ocr_model" not in st.session_state:
+        print("=========Initializing ocr========")
+        st.session_state.ocr_model = PaddleOCR(use_angle_cls=True, lang="en")
+
+    if "plates" not in st.session_state:
+        st.session_state.plates = []
+    
+    if "current_file_name" not in st.session_state:
+        st.session_state.current_file_name = ""
+
+    if "prev_file_name" not in st.session_state:
+        st.session_state.prev_file_name = ""
+
     st.title("License Plate Recognition")
 
     # Create two columns
     col1, col2 = st.columns(2)
 
     with col1:
-        st.subheader("Upload")
-        # Upload image
-        uploaded_file = st.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"])
+        st.subheader("Upload Video")
+        # Upload video
+        uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "mov", "avi"])
 
-        if uploaded_file is not None:
-            # Display image
-            image = Image.open(uploaded_file)
-            st.image(image, caption="Uploaded Image", use_column_width=True)
+        if uploaded_file:
+            if uploaded_file.name != st.session_state.current_file_name:
+                st.session_state.plates = []
 
+            st.session_state.current_file_name = uploaded_file.name
+            count = 0
+            st.video(uploaded_file, format="video/mp4")
+
+            vid_file = "data/uploaded_data/upload." + uploaded_file.name.split('.')[-1]
+            with open(vid_file, 'wb') as out:
+                out.write(uploaded_file.read())
+
+            cap = cv2.VideoCapture(vid_file)
+            start = time.time()
+            while True:
+                ret, frame = cap.read()
+
+                if not ret:
+                    cap.release()
+                    break
+
+                if count % 15 == 0: 
+                    st.session_state.plates += get_plate(st.session_state["det_model"], st.session_state["ocr_model"], frame)
+
+                count += 1
+            
     with col2:
         st.subheader("Danh sách biển số xe đọc ra được")
-        if uploaded_file is not None:
-            # Process the image (placeholder for actual processing)
-            license_plate_text = process_image(image)
-
+        if st.session_state.plates != []:
             # Display recognized license plate text
-            st.write(license_plate_text)
+            st.session_state.plates.sort()
+            for p in set(st.session_state.plates):
+                st.write(p)
         else:
             st.write("Vui lòng tải lên ảnh để nhận diện biển số.")
-
-def process_image(image):
-    # Placeholder function for image processing
-    return "ABC1234"
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,42 @@
+import numpy as np
+import time
+import cv2
+
+def get_plate(det_model, ocr_model, image):
+    # Placeholder function for image processing
+    results = det_model.predict(image, conf=0.25, iou=0.5)
+
+    cropped_imgs = crop_objects(results)
+
+    if cropped_imgs == []:
+        return []
+    
+    res = []
+    for img in cropped_imgs:
+        crp_img = np.array(img)
+        ocr_res = ocr_model.ocr(crp_img, cls=True)
+        if ocr_res[0] is not None:
+            extracted_plate = "".join(comp[-1][0] for comp in ocr_res[0])
+            extracted_plate = "".join(filter(lambda x: x.isalpha() or x.isdigit(), extracted_plate))
+            print(extracted_plate)
+            if len(extracted_plate) >= 6 and len(extracted_plate) <= 10:
+                res.append(extracted_plate)
+        
+        # cv2.imshow("test", img)
+        # cv2.waitKey(0)
+
+    return res
+
+def crop_objects(results):
+    crops = []
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Get box coordinates
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+            # Crop the image
+            crop = r.orig_img[y1:y2, x1:x2]
+            crops.append(crop)
+    return crops
